b8523f5eff4fa1eb4476be73030a4fcd891fe58d
Kuldeep <83597212+Kuldeepsinghmehra@users.noreply.github.com>
2024-04-18 11:07:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,26 +24,28 @@ function render(leads) {
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-                <button class="delete-btn" data-index="${i}">Delete</button>
+                <div class="lead-item">
+                    <a target='_blank' href='${leads[i]}'>
+                        ${leads[i]}
+                    </a>
+                    <button class="delete-btn-item" data-index="${i}">Delete</button>
+                </div>
             </li>
         `;
     }
     ulEl.innerHTML = listItems;
 
- 
-    const deleteButtons = document.querySelectorAll(".delete-btn");
-    deleteButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            const index = parseInt(button.getAttribute("data-index"));
+    ulEl.addEventListener("click", function(event) {
+        if (event.target.classList.contains("delete-btn-item")) {
+            const index = parseInt(event.target.getAttribute("data-index"));
             myLeads.splice(index, 1);
             localStorage.setItem("myLeads", JSON.stringify(myLeads));
             render(myLeads);
-        });
+        }
     });
 }
+ 
+  
 
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear();
